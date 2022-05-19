175f1645f32a3e2358a37f0825073713f6e6a9d6
mdaias <mdaias29@gmail.com>
2022-05-19 21:17:48 +0600

    get product by id

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const res = require('express/lib/response');
 
 
 // middleware
@@ -13,12 +14,36 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.SPICE_USER}:${process.env.SECRET_KEY}@cluster0.vgoc3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('db connected')
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("FreshSpice").collection("product");
+
+            //Get All Product
+        app.get('/product', async(req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query)
+            const result = await cursor.toArray();
+    
+            res.send(result);
+        })
+
+        //Get Product by Id
+        app.get('/product/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const result = await productCollection.findOne(query)
+
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 
 //Use 
