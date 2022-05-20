d5e77c0f371394a4294be72121d801de4e1ef9f7
mdaias <mdaias29@gmail.com>
2022-05-20 20:10:41 +0600

    get product by email

--- index.js
+++ index.js
@@ -20,30 +20,66 @@ async function run() {
         await client.connect();
         const productCollection = client.db("FreshSpice").collection("product");
 
-            //Get All Product
-        app.get('/product', async(req, res) => {
+        //Get All Product
+        app.get('/product', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query)
             const result = await cursor.toArray();
-    
+
             res.send(result);
         })
 
         //Get Product by Id
-        app.get('/product/:id', async(req, res)=>{
+        app.get('/product/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const result = await productCollection.findOne(query)
 
             res.send(result);
         })
 
         //post data
-        app.post('/product', async(req, res) =>{
+        app.post('/product', async (req, res) => {
             const newService = req.body;
             const result = await productCollection.insertOne(newService);
             res.send(result);
         });
+
+        // DELETE
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        //get product by email        
+        app.get('/myproduct', async (req, res) => {
+            const email = req.query.email;
+           
+            const query = {email:email};
+            const cursor = productCollection.find(query)
+            const result = await cursor.toArray();
+
+            res.send(result);
+        })
+
+        // update user
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedProduct = req.body.stock;
+            console.log(id)
+            console.log(updatedProduct)
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    stock: updatedProduct,
+                }
+            };
+            const result = await productCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
     }
     finally {
 
