5323c20efcfc71bfb6d0a669d7bedd166f9bc243
mdaias <mdaias29@gmail.com>
2022-05-21 00:55:33 +0600

    add

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const res = require('express/lib/response');
 
 
 // middleware
@@ -12,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.SPICE_USER}:${process.env.SECRET_KEY}@cluster0.vgoc3.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://freshSpice:LYqZPBKNeYtdE13F@cluster0.vgoc3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
@@ -61,6 +60,7 @@ async function run() {
             const cursor = productCollection.find(query)
             const result = await cursor.toArray();
 
+            console.log(result);
             res.send(result);
         })
 
